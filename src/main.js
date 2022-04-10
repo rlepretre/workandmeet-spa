e7c440700269f4d0139f46053628580a59f0e192
Victoria <victoria.vandeweyer@outlook.be>
2022-04-11 00:01:12 +0200

    file json

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/dist/quasar.css'
 import  VueGoogleMaps from '@fawmi/vue-google-maps'
 import store from './store'
+import router from './router'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+// Import all local custom scss
+import './assets/scss/main.scss'
 
 
 const app = createApp(App)
-
+app.use(router)
 app.use(Quasar, {
   plugins: {
   }
